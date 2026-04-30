8b80f0644f574ea2176f8a0ca8cd6c57d05b874c
kmrw0101 <164782109+kmrw0101@users.noreply.github.com>
2026-04-30 17:09:18 +0000

    Create test_setup_template.sql

---
+++ toolkit/test_setup_template.sql
@@ -0,0 +1,50 @@
+Use dbName
+
+-----------------------------------------
+-- clean up any previous runs of the test
+-----------------------------------------
+
+DELETE FROM [dbName].[dbo].[tableName] -- source
+WHERE ACCT_NO IN ('1000001')
+
+-----------------------------------------
+-- setup test scenarios
+-----------------------------------------
+
+DECLARE @Date1 DATE;
+SET @Date1 = '2026-02-25';
+
+DECLARE @ID INT;
+SET @ID = COALESCE((SELECT MAX(ID) FROM dbo.tableName), 0) + 1
+
+INSERT INTO [dbName].[dbo].[tableName]
+   (ID,
+      DATE_FIELD,
+      CHANGE_TYPE,
+      ACCT_NO,
+      Col4,
+      Col5)
+VALUES
+    (@ID,
+       @Date1,
+       'A', --ADD
+     '1000001',
+       'Col4',
+       'Col5')
+
+/*
+ continue to build out testing for all scenarios:
+ record behavior for multi-day tests with ADDS, CHANGES, VOIDS 
+ and all transformations for each column. Test happy path 
+ and negative edge case testing to ensure data is mapped 
+ appropriately based on what is in the source-to-target or 
+ metadata documents provided.
+ */
+
+-----------------------------------------
+-- review inserted data
+-----------------------------------------
+SELECT '[dbName].[dbo].[tableName]'
+SELECT ID, DATE_FIELD, CHANGE_TYPE, ACCT_NO, Col4, Col5
+FROM [dbName].[dbo].[tableName]
+WHERE ACCT_NO IN ('1000001')
